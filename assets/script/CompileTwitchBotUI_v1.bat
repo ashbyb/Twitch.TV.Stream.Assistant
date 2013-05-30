1dd4e2ec9937994e84e1fd4f2c21f5fd5839bdb0
Brendan E Ashby <ashbyb@rpi.edu>
2013-05-30 18:34:12 -0400

    Edited spelling in script file

--- assets/script/CompileTwitchBotUI_v1.bat
+++ assets/script/CompileTwitchBotUI_v1.bat
@@ -3,7 +3,7 @@
 REM Clear the screen before we start
 cls
 
-REM Written by Siye, Copyright 2013 | All Rights Reserved
+REM Written by ashbyb/student/Siye, Copyright 2013 | All Rights Reserved
 
 REM Set the color of the CMD terminal
 color 0A
@@ -36,7 +36,7 @@ Echo +---------------------------------------------+
 
 "C:\Python27\Lib\site-packages\PyQt4\uic\pyuic.py" "C:\Users\morro_000\workspace\TwitchChatBot\AccountManagementWindow_v1.ui" -o "C:\Users\morro_000\workspace\TwitchChatBot\AccountManagementWindow_v1.py"
 
-REM Compile the icons recourse file ".ui" to a valid ".py" file so the GUI can use the contained assets
+REM Compile the icons resource file ".qrc" to a valid ".py" file so the GUI can use the contained assets
 Echo +---------------------------------+
 Echo ^| Now Compiling Image Assets...   ^|
 Echo +---------------------------------+
@@ -54,6 +54,3 @@ cls
 color
 
 Rem EOF
-
-
-AccountManagementWindow_v1
