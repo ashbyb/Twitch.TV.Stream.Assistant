262cafa26c319a6641016df55b4ed03c3ae7becc
FireElementalNE <FireElemental.NE@gmail.com>
2013-05-31 10:56:57 -0400

    Ported to linux

---
+++ assets/script/CompileTwitchBotUI_v1.sh
@@ -0,0 +1,61 @@
+#Clear the screen before we start
+#clear
+
+#Written by FirelementalNE ported from ashbyb/student/Siye, Copyright 2013 | All Rights Reserved
+sudo echo " aeafesafs "
+PATH1=/usr/lib/python2.7/dist-packages/PyQt4/uic/pyuic.py
+TWPYDIR=~/Twitch.TV.Stream.Assistant/src
+TWUIDIR=~/Twitch.TV.Stream.Assistant/assets/ui
+MAINUI=${TWUIDIR}/MainWindow_v1.ui
+MAINPY=${TWPYDIR}/MainWindow_v1.py
+ACCTUI=${TWUIDIR}/AccountManagementWindow_v1.ui
+ACCTPY=${TWPYDIR}/AccountManagementWindow_v1.py
+MEDIAQRC=${TWPYDIR}/media.qrc
+MEDIAPY=${TWPYDIR}/media_rc.py
+
+#Show initial header dialog
+echo "+---------------------------------------+"
+echo "|       Siye\'s Twitch Bot Compiler     |"
+echo "|                                       |"
+echo "|               [Notice]                |"
+echo "| All paths in this folder are absolute |"
+echo "|   Modify them to work on your system  |"
+echo "|                                       |"
+echo "|   ... Press any key to continue ...   |"                   
+echo "+---------------------------------------+"
+
+# Wait for user input
+read READIT1
+
+#Compile the Main window ".ui" file to a valid ".py" file
+echo "+------------------------------+"
+echo "| Now Compiling Main Window... |"
+echo "+------------------------------+"
+
+sudo python ${PATH1} ${MAINUI} -o ${MAINPY}
+
+#Compile the Account Management window ".ui" file to a valid ".py" file
+echo "+---------------------------------------------+"
+echo "| Now Compiling Accounts Management Window... |"
+echo "+---------------------------------------------+"
+
+sudo python ${PATH1} ${ACCTUI} -o ${ACCTPY}
+
+# Compile the icons resource file ".qrc" to a valid ".py" file so the GUI can use the contained assets
+echo "+---------------------------------+"
+echo "| Now Compiling Image Assets...   |"
+echo "+---------------------------------+"
+
+sudo python ${PATH1} ${MEDIAQRC} -o ${MEDIAPY}
+
+echo "+----------------------+"
+echo "|  Finished Compiling! |"
+echo "+----------------------+"
+
+echo "Press [Enter] key to Finish..."
+read READIT
+
+#Some final clean up (clear screen)
+#clear
+
+#EOF
